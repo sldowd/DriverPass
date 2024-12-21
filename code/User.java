595466d784ffe451d2d9cd941bef2e0dfe1ce178
sdowd <sarahlynnedowd@gmail.com>
2024-12-21 18:22:07 -0500

    defined User class

---
+++ code/User.java
@@ -0,0 +1,9 @@
+
+public class User {
+    private static String username;
+    private static String password;
+    private static String email;
+    private static String phone;
+    private static String address;
+
+}
